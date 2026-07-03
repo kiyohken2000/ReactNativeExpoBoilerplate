ed470aa950d245a45a983b380b5660a238826d64
kiyohken2000 <www.broadwell.com@gmail.com>
2026-07-03 11:32:24 +0900

    [upgrade]Expo SDK 57

--- src/routes/navigation/stacks/ProfileStacks.js
+++ src/routes/navigation/stacks/ProfileStacks.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { createStackNavigator } from '@react-navigation/stack'
+import FontIcon from 'react-native-vector-icons/FontAwesome'
 import { navigationProps } from './navigationProps/navigationProps'
 import GradientHeader from '../../../components/GradientHeader'
+import { showToast } from '../../../utils/showToast'
+import { colors } from '../../../theme'
 
 import Profile from '../../../scenes/profile/Profile'
 import Details from '../../../scenes/details/Details'
@@ -20,6 +24,16 @@ export const ProfileStacks = () => {
         options={({ navigation }) => ({
           title: 'Profile',
           headerBackground: () => <GradientHeader />,
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {
+                showToast({ title: 'Hello', body: 'Header button pressed' })
+              }}
+              style={{ paddingHorizontal: 15 }}
+            >
+              <FontIcon name="bell" size={20} color={colors.white} />
+            </TouchableOpacity>
+          ),
         })}
       />
       <Stack.Screen
